e0b6b30e4cda9a097d9bdb641e320a25474176de
krchandarana <kashyapchandarana49@gmail.com>
2021-07-02 00:11:39 +0530

    Files added

---
+++ edit.js
@@ -0,0 +1,21 @@
+var allowOrigin = "https://cors-anywhere.herokuapp.com";
+var serverURL = "http://dalwadee.com/kc";
+var gold999RateChange = 0;
+var gold22kRateChange = -1000;
+var silverRateChange = 0;
+
+$.ajax({
+    type: 'GET',
+    url: serverURL + '/getRateChange.php',
+    dataType: "json",
+    async: false,
+    crossDomain: true,
+    processData: false,
+    cache: false,
+    success: function (response) {
+        gold999RateChange = parseInt(response.gold999);
+        gold22kRateChange = parseInt(response.gold22k);
+        silverRateChange = parseInt(response.silver);
+    }
+});
+
